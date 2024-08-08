e546a1a62f9124616df30323e530ee616b8c9b84
KatianaHanisch <katianahanisch@gmail.com>
2024-08-08 14:43:27 -0400

    fix: adjustments to private routes

--- src/routes/private.ts
+++ src/routes/private.ts
@@ -2,10 +2,6 @@ import { Router } from "express";
 
 import { privateController } from "../controllers/private-controller";
 import { ensureAdmin, ensureAuth } from "../middlewares/auth-middleware";
-import { Router } from "express";
-
-import { privateController } from "../controllers/private-controller";
-import { ensureAdmin, ensureAuth } from "../middlewares/auth-middleware";
 
 export const privateRouter = Router();
 
